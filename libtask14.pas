e6c0c6ed71947dd09cd494c1bb62dcc4c2de274e
Kulagin Fyodor <kulagin.f.p@yandex.ru>
2021-10-21 09:10:38 +0300

    add LaterInYear

--- libtask14.pas
+++ libtask14.pas
@@ -6,5 +6,11 @@ begin
   if (year mod 4 = 0) and (year mod 100 <> 0) or (year mod 400 = 0) then result := true;
 end;
 
+function LaterInYear(day1, day2, mouth1, mouth2:integer):
+begin
+  result := false;
+  if (mouth1 > mouth2) or ((mouth1 = mouth2) and (day1 > day2)) then result := true;
+end;
+
 begin
 end.
