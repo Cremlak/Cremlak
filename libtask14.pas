0b674412684e2709deb1006b0d9dd147f1a69780
Kulagin Fyodor <kulagin.f.p@yandex.ru>
2021-10-21 09:18:16 +0300

    add DaysInYear and DaysInYearRange

--- libtask14.pas
+++ libtask14.pas
@@ -1,15 +1,28 @@
 ﻿unit libtask14;
 
-function IsLeapYear:boolean;
+function IsLeapYear(year:integer):boolean;
 begin
-  var year := DateTime.Now.Year;
   if (year mod 4 = 0) and (year mod 100 <> 0) or (year mod 400 = 0) then result := true;
 end;
 
-function LaterInYear(day1, day2, mouth1, mouth2:integer):
+function LaterInYear(day1, day2, mouth1, mouth2:integer):boolean;
 begin
   result := false;
   if (mouth1 > mouth2) or ((mouth1 = mouth2) and (day1 > day2)) then result := true;
+end;
+
+function DaysInYear(year: integer):integer;
+begin
+  if IsLeapYear(year) then result := 366
+  else result := 365;
+end;
+
+function DaysInYearRange(year1, year2: integer):integer;
+begin
+  for var i := year1 to year2 do
+  begin
+    result += DaysInYear(i);
+  end;
 end;
 
 begin
