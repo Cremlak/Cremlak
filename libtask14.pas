19de8dd25f4a47c38449621ed921a6d6763c9bde
Kulagin Fyodor <kulagin.f.p@yandex.ru>
2021-10-21 08:48:58 +0300

    Add function IsLeapYear

--- libtask14.pas
+++ libtask14.pas
@@ -2,7 +2,8 @@
 
 function IsLeapYear:boolean;
 begin
-  result:=false;
+  var year := DateTime.Now.Year;
+  if (year mod 4 = 0) and (year mod 100 <> 0) or (year mod 400 = 0) then result := true;
 end;
 
 begin
